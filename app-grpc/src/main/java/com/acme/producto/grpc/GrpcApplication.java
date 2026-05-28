4d9e9420ac4ad180ba03d6f403c5fcc5613a47a2
zyozmen <61213472+zyozmen@users.noreply.github.com>
2026-05-28 13:12:27 -0500

    Final changes

--- app-grpc/src/main/java/com/acme/producto/grpc/GrpcApplication.java
+++ app-grpc/src/main/java/com/acme/producto/grpc/GrpcApplication.java
@@ -2,8 +2,12 @@ package com.acme.producto.grpc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.acme.producto")
+@EnableJpaRepositories(basePackages = "com.acme.producto")
+@EntityScan(basePackages = "com.acme.producto")
 public class GrpcApplication {
 
     public static void main(String[] args) {
--- app-grpc/src/main/java/com/acme/producto/infrastructure/persistence/repository/SpringDataProductoRepository.java
+++ app-grpc/src/main/java/com/acme/producto/infrastructure/persistence/repository/SpringDataProductoRepository.java
@@ -2,6 +2,8 @@ package com.acme.producto.infrastructure.persistence.repository;
 
 import com.acme.producto.infrastructure.persistence.entity.ProductoJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SpringDataProductoRepository extends JpaRepository<ProductoJpaEntity, String> {
 }
